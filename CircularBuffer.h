ea00030fb4537f7988f0c2137e25c967594068e7
hunter2804 <54524489+hunter2804@users.noreply.github.com>
2019-09-27 15:19:54 +0530

    update an ordinary structure to packed structure

---
+++ CircularBuffer.h
@@ -0,0 +1,43 @@
+#ifndef __CIRCULAR_BUFFER_H__
+#define __CIRCULAR_BUFFER_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define debug printf
+
+#define Buffer			packet.buffer
+#define Length			packet.bufferLength
+#define Head			packet.head
+#define Tail			packet.tail
+#define bufferFull		packet.isBufferFull
+
+class CircularBuffer{
+	private:		
+		struct __attribute__((packed)) circularBuffer{
+			uint8_t *buffer;
+			uint32_t bufferLength;
+			uint32_t head;
+			uint32_t tail;
+			bool isBufferFull;
+		}packet;
+
+		bool initialized;
+		void flushPacket(const uint32_t length);
+	public:
+		CircularBuffer(const uint32_t dataLength);
+		~CircularBuffer(void);
+
+		bool write(const uint8_t data);
+		bool write(const uint8_t *data, uint32_t length);
+
+		const uint8_t read(void);
+
+		bool remove(const uint32_t length=1);
+
+		const uint32_t getSize(void);
+};
+
+#endif
+
